fa1eaf89336c99021bea955d6c19fa1155db048f
Mateus <creztfallen@M.S.M>
2023-02-23 14:04:33 -0300

    feat::sparkles: add connection to database

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-    "github.com/gofiber/fiber/v2"
+	"github.com/creztfallen/dnd-spells-api/configs"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.JSON(&fiber.Map{"data": "Vai, come ainnn!"})
-    })
+	configs.ConnectDB()
 
-    app.Listen(":6000")
+	app.Listen(":6000")
 }
